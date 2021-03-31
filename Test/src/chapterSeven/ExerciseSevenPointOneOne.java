f198efd486c32dfcf17948863b5eb818ea1caa90
Oluwatoni Adeniyi <toniadeniyi14@gmail.com>
2021-03-31 23:54:38 +0100

    git commit -m "Chapter Seven"

--- Test/src/chapterSeven/ExerciseSevenPointOneOne.java
+++ Test/src/chapterSeven/ExerciseSevenPointOneOne.java
@@ -1,4 +1,36 @@
 package chapterSeven;
 
 public class ExerciseSevenPointOneOne {
+    private int [] input;
+    private int [] input2;
+
+    public void setElements(int[] input) {
+        for (int i = 0; i < input.length; i++) {
+        }
+        this.input = input;
+    }
+
+    public int[] getElements() {
+        return input;
+    }
+
+    public void incrementElements(int[] input2) {
+        for (int i = 0; i < input2.length; i++) {
+            input2[i] = i + 1;
+        }
+        this.input2 = input2;
+    }
+
+    public int[] getIncrementedElements() {
+        return input2;
+    }
+
+    public void bestScores(int[] input2) {
+        getIncrementedElements();
+        input2.
+    }
+
+    public int[] displayBestScores() {
+        return;
+    }
 }
--- Test/test/chapterSeven/ExerciseSevenPointOneOneTest.java
+++ Test/test/chapterSeven/ExerciseSevenPointOneOneTest.java
@@ -2,16 +2,41 @@ package chapterSeven;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ExerciseSevenPointOneOneTest {
+    ExerciseSevenPointOneOne array;
 
     @BeforeEach
     void setUp() {
+        array = new ExerciseSevenPointOneOne();
     }
 
     @AfterEach
     void tearDown() {
+        array = null;
+    }
+
+    @Test
+    void testThatElementsOfAnArrayHaveZeroValue(){
+        int [] input = new int[10];
+        array.setElements(input);
+        assertArrayEquals(new int []{0,0,0,0,0,0,0,0,0,0}, array.getElements());
+    }
+
+    @Test
+    void testThatOneCanBeAddedToAllElementsOfTheArray(){
+        int [] input2 = new int[15];
+        array.incrementElements(input2);
+        assertArrayEquals(new int[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}, array.getIncrementedElements());
+    }
+
+    @Test
+    void testToDisplayBestFiveScoresOfTheArray(){
+        int [] input2 = new int[15];
+        array.bestScores(input2);
+        assertArrayEquals(new int[]{11,12,13,14,15}, array.displayBestScores());
     }
 }
