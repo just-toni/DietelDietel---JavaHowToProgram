5fee785461064664d62e2a49f84642cae170a76e
Oluwatoni Adeniyi <toniadeniyi14@gmail.com>
2021-03-31 23:10:34 +0100

    git commit -m "Chapter Seven"

---
+++ Test/test/chapterSeven/ExerciseSevenPointOneOneTest.java
@@ -0,0 +1,17 @@
+package chapterSeven;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExerciseSevenPointOneOneTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
