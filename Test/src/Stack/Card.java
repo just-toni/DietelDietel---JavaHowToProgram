ea11aae35a3e4a5ecae314d4689220b87cc705fe
Oluwatoni Adeniyi <toniadeniyi14@gmail.com>
2021-04-08 04:29:59 +0100

    git commit -m "Exercises"

--- Test/src/Stack/Card.java
+++ Test/src/Stack/Card.java
@@ -1,4 +1,17 @@
 package Stack;
 
 public class Card {
+
+    private final String face;
+    private final String suits;
+
+    public Card(String face, String suits) {
+        this.face = face;
+        this.suits = suits;
+    }
+
+    @Override
+    public String toString() {
+        return face + " of " + suits;
+    }
 }
