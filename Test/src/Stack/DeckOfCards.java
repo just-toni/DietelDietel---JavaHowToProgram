d96baca8ea82eb01ee10f42c73119ecd9584f4e7
Oluwatoni Adeniyi <toniadeniyi14@gmail.com>
2021-04-08 04:37:21 +0100

    git commit -m "Deck Of Cards"

--- Test/src/Stack/DeckOfCards.java
+++ Test/src/Stack/DeckOfCards.java
@@ -1,23 +1,28 @@
-package Stack;public class DeckOfCards {
+package Stack;
+
+public class DeckOfCards {
 
     private String[] faces = {"Ace", "Deuce", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
     private String[] suits = {"Spades", "Hearts", "Diamonds", "Clubs"};
     private Card [] deckOfCards = new Card[52];
 
     public void generateCardDeck() {
-        for (int k = 0; k < deckOfCards.length; k++) {
-            for (int i = 0; i < faces.length; i++) {
-                for (int j = 0; j < suits.length; j++) {
-                    Card card = new Card(faces[i], suits[j]);
-                    deckOfCards[k] = card;
-                    System.out.println(deckOfCards[k]);
-                }
+        for (int i = 0; i < faces.length; i++) {
+            for (int j = 0; j < suits.length; j++) {
+                Card card = new Card(faces[i], suits[j]);
+                System.out.println(card);
             }
         }
-
     }
 
-    public Card[] getCardDeck() {
-        return deckOfCards;
-    }
 }
+
+//for (int k = 0; k < deckOfCards.length; k++) {
+//            for (int i = 0; i < faces.length; i++) {
+//                for (int j = 0; j < suits.length; j++) {
+//                    Card card = new Card(faces[i], suits[j]);
+//                    deckOfCards[k] = card;
+//                    System.out.println(deckOfCards[k]);
+//                }
+//            }
+//        }
