b55bd1e5ac811a7e342d4b260a1e6a3a71960e48
Oluwatoni Adeniyi <toniadeniyi14@gmail.com>
2021-04-13 10:15:13 +0100

    git commit -m "Chapter Eight"

--- Test/test/chapterEight/TimeTrialTest.java
+++ Test/test/chapterEight/TimeTrialTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class TimeTrialTest {
     TimeTrial trial;
@@ -31,5 +32,46 @@ class TimeTrialTest {
         int minute = 45;
         trial.setMinute(minute);
         assertEquals(45, trial.getMinute());
+    }
+
+    @Test
+    void testThatSecondsCanBeSet(){
+        int second = 34;
+        trial.setSeconds(second);
+        assertEquals(34, trial.getSeconds());
+    }
+
+    @Test
+    void testThatWrongHourEnteredCanShowErrorMessage(){
+        int hour = 45;
+        assertThrows(IllegalArgumentException.class, ()-> trial.setHour(hour));
+    }
+
+    @Test
+    void testThatWrongMinuteEnteredCanShowErrorMessage(){
+        int minute = 145;
+        assertThrows(IllegalArgumentException.class, ()-> trial.setMinute(minute));
+    }
+
+    @Test
+    void testThatWrongSecondEnteredCanShowErrorMessage(){
+        int second = 145;
+        assertThrows(IllegalArgumentException.class, ()-> trial.setSeconds(second));
+    }
+    
+    @Test
+    void testToDisplayTimeInUniversalTimeFormat(){
+        trial.setHour(3);
+        trial.setMinute(45);
+        trial.setSeconds(23);
+        assertEquals("03:45:23", trial.toUniversalTimeFormat());
+    }
+
+    @Test
+    void testToDisplayTimeInStandardTimeFormat(){
+        trial.setHour(6);
+        trial.setMinute(15);
+        trial.setSeconds(43);
+        assertEquals("06:15:43 AM", trial.toStandardTimeFormat());
     }
 }
