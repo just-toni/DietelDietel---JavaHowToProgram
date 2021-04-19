ef17965518541f599c811851443cd9fef58d1f62
Oluwatoni Adeniyi <toniadeniyi14@gmail.com>
2021-04-19 17:24:44 +0100

    git commit - m "My Diary"

---
+++ Test/src/ourDiary/User.java
@@ -0,0 +1,20 @@
+package ourDiary;
+
+public class User {
+
+    private final String userName;
+    private final Diary diary;
+
+    public User(String userName, Diary userDiary) {
+        diary = userDiary;
+        this.userName = userName;
+    }
+
+    public Diary getDiary(){
+        return diary;
+    }
+
+    public void createEntry(String entryBody) {
+        diary.addEntry(entryBody);
+    }
+}
