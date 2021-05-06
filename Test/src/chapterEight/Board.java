21b186135a2ef2abdc30bb92f156a5fcc6fe135b
Oluwatoni Adeniyi <toniadeniyi14@gmail.com>
2021-05-06 10:20:32 +0100

    git commit -m "TicTacToe"

---
+++ Test/src/chapterEight/Board.java
@@ -0,0 +1,30 @@
+package chapterEight;
+
+import java.util.Arrays;
+
+public class Board {
+
+    private GameValue[][] grid;
+
+    public Board(int gridNumber) {
+        grid = new GameValue[gridNumber][gridNumber];
+        for(int row = 0; row < grid.length; row++){
+            Arrays.fill(grid[row], GameValue.EMPTY);
+        }
+    }
+
+    public GameValue[][] getGrid() {
+        return grid;
+    }
+
+    public void visualizeBoard() {
+        GameValue result;
+        for (int row = 0; row < grid.length; row++) {
+            for (int column = 0; column < grid[row].length; column++) {
+                result = grid[row][column];
+                System.out.println(result + " ");
+            }
+            System.out.println();
+        }
+    }
+}
