192dc9d795e9b25a3f3f4ab74018d76f095e2839
Oluwatoni Adeniyi <toniadeniyi14@gmail.com>
2021-05-08 15:33:59 +0100

    git commit -m "Queue"

--- Test/test/Queue/QueueTest.java
+++ Test/test/Queue/QueueTest.java
@@ -1,5 +1,7 @@
 package Queue;
 
+import com.QueueExceptions.QueueOverflowException;
+import com.QueueExceptions.QueueUnderflowException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -37,7 +39,7 @@ class QueueTest {
     }
 
     @Test
-    void dequeueOneElement(){
+    void dequeueOneElement() throws QueueUnderflowException {
         queue.enqueue(3);
         queue.enqueue(6);
         queue.enqueue(7);
@@ -49,22 +51,91 @@ class QueueTest {
     }
 
     @Test
-    void findTotalNumberOfElements(){
+    void findTotalNumberOfElements() throws QueueUnderflowException {
         queue.enqueue(3);
         queue.enqueue(6);
         queue.enqueue(7);
         queue.enqueue(89);
         assertEquals(4, queue.getTotalNumberOfElements());
+        queue.dequeue();
+        assertEquals(3, queue.getTotalNumberOfElements());
     }
 
     @Test
-    void checkIfQueueIsEmpty(){
-        queue.enqueue(3);
+    void checkIfQueueIsEmpty() throws QueueUnderflowException {
+        queue.enqueue(76);
         queue.enqueue(6);
         queue.enqueue(7);
         queue.dequeue();
         queue.dequeue();
         queue.dequeue();
+        assertEquals(0, queue.getTotalNumberOfElements());
         assertTrue(queue.isEmpty());
+    }
+
+    @Test
+    void queueUnderflow() throws QueueUnderflowException {
+        queue.enqueue(76);
+        queue.enqueue(6);
+        queue.enqueue(7);
+        queue.dequeue();
+        queue.dequeue();
+        queue.dequeue();
+        assertEquals(0, queue.getTotalNumberOfElements());
+        try{
+            queue.dequeue();
+        }
+        catch (QueueUnderflowException ex){
+            System.out.println("Queue underflow");
+        }
+        assertThrows(QueueUnderflowException.class, ()-> queue.dequeue());
+    }
+
+    @Test
+    void checkIfQueueIsFull() throws QueueOverflowException {
+        queue.enqueue(76);
+        queue.enqueue(6);
+        queue.enqueue(7);
+        queue.enqueue(36);
+        queue.enqueue(16);
+        queue.enqueue(71);
+        queue.enqueue(763);
+        queue.enqueue(60);
+        assertEquals(8, queue.getTotalNumberOfElements());
+        assertTrue(queue.isFull());
+    }
+
+    @Test
+    void queueOverflow() throws QueueOverflowException {
+        queue.enqueue(76);
+        queue.enqueue(6);
+        queue.enqueue(7);
+        queue.enqueue(36);
+        queue.enqueue(16);
+        queue.enqueue(71);
+        queue.enqueue(763);
+        queue.enqueue(60);
+        assertEquals(8, queue.getTotalNumberOfElements());
+        try{
+            queue.enqueue(75);
+        }
+        catch (QueueOverflowException ex){
+            System.out.println("Queue overflow");
+        }
+        assertThrows(QueueOverflowException.class, ()-> queue.enqueue(75));
+    }
+
+    @Test
+    void test(){
+        Queue toniQueue = new Queue(5);
+        toniQueue.enqueue(4);
+        toniQueue.enqueue(3);
+        toniQueue.enqueue(6);
+        toniQueue.enqueue(2);
+        toniQueue.dequeue();
+        toniQueue.enqueue(8);
+        toniQueue.enqueue(2);
+        assertEquals(3, toniQueue.peekFirst());
+
     }
 }
