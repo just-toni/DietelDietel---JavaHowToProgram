68d428fdc8f176e1795012b5629ca60fb0b1c3fa
Oluwatoni Adeniyi <toniadeniyi14@gmail.com>
2021-05-08 15:32:59 +0100

    git commit -m "Queue"

---
+++ Test/src/com/QueueExceptions/QueueOverflowException.java
@@ -0,0 +1,7 @@
+package com.QueueExceptions;
+
+public class QueueOverflowException extends QueueExceptions {
+    public QueueOverflowException(String errorMessage) {
+        super(errorMessage);
+    }
+}
