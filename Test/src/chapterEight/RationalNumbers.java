9b4c169b8cda8b6024f8fe1d9e28ff8f5d6928b1
Oluwatoni Adeniyi <toniadeniyi14@gmail.com>
2021-05-09 20:56:28 +0100

    git commit -m "Chapter Eight"

--- Test/src/chapterEight/RationalNumbers.java
+++ Test/src/chapterEight/RationalNumbers.java
@@ -1,15 +1,17 @@
 package chapterEight;
 
+import chapterSix.GCD;
+
 public class RationalNumbers {
     private int denominator;
     private int numerator;
 
     public RationalNumbers(int numerator, int denominator) {
-        numerator = Math.abs(numerator);
-        denominator = Math.abs(denominator);
-        int gcd = calculateGCD(numerator, denominator);
-        this.numerator = numerator/gcd;
-        this.denominator = denominator/gcd;
+        int gcd = GCD.calculateGCD(numerator, denominator);
+        if (denominator < 0) this.numerator = -1 * numerator/ gcd;
+        else if (denominator > 0) this.numerator = numerator/gcd;
+        else this.numerator = 0;
+        this.denominator = Math.abs(denominator) /  gcd;
     }
 
     public int getNumerator() {
@@ -20,31 +22,32 @@ public class RationalNumbers {
         return denominator;
     }
 
-    public static RationalNumbers add(RationalNumbers rationalNumbers1, RationalNumbers rationalNumbers2){
-        int lcm = rationalNumbers1.getDenominator() * rationalNumbers2.getDenominator();
-        System.out.println("lcm is " + lcm);
-        int firstProduct = lcm / rationalNumbers1.getDenominator() * rationalNumbers1.getNumerator();
-        System.out.println("first product is " + firstProduct);
-        int secondProduct = lcm / rationalNumbers2.getDenominator() * rationalNumbers2.getNumerator();
-        System.out.println("second product is " + secondProduct);
-        int result = firstProduct + secondProduct;
-        System.out.println("result is " + result);
-        System.out.println("Final Result is: " + calculateGCD(rationalNumbers1.getDenominator(), rationalNumbers2.getDenominator()) * result + "/" +lcm);
-        return calculateGCD(rationalNumbers1.getDenominator(), rationalNumbers2.getDenominator()) * result + "/" +lcm;
+    public static RationalNumbers add(RationalNumbers rationalNumbers1, RationalNumbers rationalNumbers2) {
+        int denominator = rationalNumbers1.getDenominator() * rationalNumbers2.getDenominator();
+        int numerator = rationalNumbers1.getNumerator() * rationalNumbers2.getDenominator() +
+                rationalNumbers1.getDenominator() * rationalNumbers2.getNumerator();
+        return new RationalNumbers(numerator, denominator);
     }
 
-    private static int calculateGCD(int numerator, int denominator) {
-        int max = Math.max(numerator, denominator);
-        numerator = Math.abs(numerator);
-        denominator = Math.abs(denominator);
-        int gcd = 0;
-        for(int i = 1; i < max; i++){
-            if(numerator % i == 0 && denominator % i == 0){
-                gcd = i;
-            }
-        }
-        System.out.println(gcd);
-        return gcd;
+    public static RationalNumbers subtract(RationalNumbers rationalNumbers1, RationalNumbers rationalNumbers2) {
+        int denominator = rationalNumbers1.getDenominator() * rationalNumbers2.getDenominator();
+        int numerator = rationalNumbers1.getNumerator() * rationalNumbers2.getDenominator() -
+                rationalNumbers1.getDenominator() * rationalNumbers2.getNumerator();
+        return new RationalNumbers(numerator, denominator);
     }
 
-}
+    public static RationalNumbers multiply(RationalNumbers rationalNumbers1, RationalNumbers rationalNumbers2) {
+        int denominator = rationalNumbers1.getDenominator() * rationalNumbers2.getDenominator();
+        int numerator = rationalNumbers1.getNumerator() * rationalNumbers2.getNumerator();
+        return new RationalNumbers(numerator, denominator);
+    }
+
+    public static RationalNumbers divide(RationalNumbers rationalNumbers1, RationalNumbers rationalNumbers2) {
+        int newRationalNumerator2 = rationalNumbers2.getDenominator();
+        int newRationalDenominator2 = rationalNumbers2.getNumerator();
+        int denominator = rationalNumbers1.getDenominator() * newRationalDenominator2;
+        int numerator = rationalNumbers1.getNumerator() * newRationalNumerator2;
+        return new RationalNumbers(numerator, denominator);
+    }
+
+   }
