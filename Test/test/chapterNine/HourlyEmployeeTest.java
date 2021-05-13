380b6013071ef6055ef605a79d0c1cacea5e2a01
Oluwatoni Adeniyi <toniadeniyi14@gmail.com>
2021-05-14 00:34:29 +0100

    git commit -m "Chapter Nine"

---
+++ Test/test/chapterNine/HourlyEmployeeTest.java
@@ -0,0 +1,17 @@
+package chapterNine;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HourlyEmployeeTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
