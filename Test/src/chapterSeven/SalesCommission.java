f8b4ad7da21c985071f78bbcc88072a6357922cc
Oluwatoni Adeniyi <toniadeniyi14@gmail.com>
2021-06-11 19:07:19 +0100

    Added Sales Commission

---
+++ Test/src/chapterSeven/SalesCommission.java
@@ -0,0 +1,4 @@
+package chapterSeven;
+
+public class SalesCommission {
+}
---
+++ Test/test/chapterSeven/SalesCommissionTest.java
@@ -0,0 +1,19 @@
+package chapterSeven;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SalesCommissionTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+
+}
