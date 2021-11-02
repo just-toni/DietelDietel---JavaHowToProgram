04c4f1eee6fcd18924610053b2c55fabb3b954d5
Oluwatoni Adeniyi <toniadeniyi14@gmail.com>
2021-11-02 19:18:21 +0100

    Chapter Fourteen

--- Test/src/chapterFourteen/DateFormat.java
+++ Test/src/chapterFourteen/DateFormat.java
@@ -1,9 +1,14 @@
 package chapterFourteen;
 
 public class DateFormat {
-    public String convertDate(String date) {
+    public void convertDate(String date) {
         if(validateDate(date)){
-
+            String[] splitDate = date.split("/");
+            int[] date1 = new int[3];
+            for (int i = 0; i < splitDate.length; i++) {
+                date1[i] = Integer.parseInt(splitDate[i]);
+            }
+            System.out.println(months[date1[1] - 1] + " " + date1[0] + ", " + date1[2]);
         }
     }
 
@@ -16,6 +21,11 @@ public class DateFormat {
             "May", "June", "July", "August",
             "September", "October", "November", "December"
     };
+
+    public static void main(String[] args) {
+        DateFormat dateFormat = new DateFormat();
+        dateFormat.convertDate("24/03/2001");
+    }
 
 
 }
